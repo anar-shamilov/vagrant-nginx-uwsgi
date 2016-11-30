cba8aba340693dc86003ebf075de3ce7de094237
Jamal Shahverdiev <jamal.shahverdiev@gmail.com>
2016-11-30 16:42:20 +0400

    Update app.py

--- roles/webserver/templates/app.py
+++ roles/webserver/templates/app.py
@@ -1,3 +1,3 @@
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1 style='color:blue'>Hello Datanyze!</h1>"]
+    return ["<h1 style='color:blue'>Hello ADA University!</h1>"]
